3183db452f853dbc768a244bcc07a4abcda4f67a
vladimir <128848265+arhixia@users.noreply.github.com>
2024-10-06 17:59:36 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,199 +2,184 @@ from datetime import datetime
 import telebot
 from telebot import types
 from sqlalchemy.orm import sessionmaker
-from src.database import engine, SessionLocal  # Импортируйте свой файл с настройками базы данных
-from src.models import Elective, Review  # Импортируйте вашу модель Elective
+from src.database import engine
+from src.models import Elective, Review
 from sqlalchemy import select
 from src.config import TOKEN
 from sqlalchemy.orm import joinedload
-
+from googletrans import Translator
 
 bot = telebot.TeleBot(TOKEN, parse_mode='None')
+translator = Translator()
 
 user_elective_selections = {}
-last_selection = {}  # Храним последнюю выбранную категорию элективов
 user_languages = {}
 user_reviews = {}
 
-# Создание сессии базы данных
 Session = sessionmaker(bind=engine)
+
+
+def translate_message(text, language):
+    return translator.translate(text, dest='en').text if language == 'en' else text
+
+
+def send_translated_message(chat_id, message, language):
+    bot.send_message(chat_id, translate_message(message, language))
+
+
+def get_language(chat_id):
+    return user_languages.get(chat_id, 'ru')
+
+
+def show_electives(electives, language):
+    response = "Доступные элективы:\n" + "\n".join(f"- {elective.unique_id}: {elective.name}" for elective in electives)
+    return translate_message(response, language)
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     chat_id = message.chat.id
-    # Устанавливаем язык по умолчанию (если он еще не был установлен)
-    if chat_id not in user_languages:
-        user_languages[chat_id] = 'ru'  # По умолчанию устанавливаем русский язык
+    language = user_languages.setdefault(chat_id, 'ru')
+    welcome_message = (
+        "Привет! 👋\n\n"
+        "Я — бот, который поможет тебе выбрать элективный курс в нашем вузе. Вот что я могу:\n"
+        "- Посмотреть аннотации к элективам\n"
+        "- Узнать последние отзывы от студентов\n"
+        "- Оставить свой отзыв о курсе\n"
+        "- Выбрать язык общения (Русский или English)\n\n"
+        "Чтобы начать, используй команду /electives для просмотра доступных курсов или /language для изменения языка общения."
+    )
+    send_translated_message(chat_id, welcome_message, language)
 
-    language = user_languages[chat_id]  # Получаем язык пользователя
 
-    if language == 'ru':
-        bot.send_message(chat_id,
-            "Привет! 👋\n\n"
-            "Я — бот, который поможет тебе выбрать элективный курс в нашем вузе. Вот что я могу:\n"
-            "- Посмотреть аннотации к элективам\n"
-            "- Узнать последние отзывы от студентов\n"
-            "- Оставить свой отзыв о курсе\n"
-            "- Выбрать язык общения (Русский или English)\n\n"
-            "Чтобы начать, используй команду /electives для просмотра доступных курсов или /language для изменения языка общения. "
-            "Нажмите на меню в левом нижнем углу и посмотрите возможные команды."
-        )
-    else:
-        bot.send_message(chat_id,
-            "Hello! 👋\n\n"
-            "I am a bot that will help you choose an elective course at our university. Here’s what I can do:\n"
-            "- View annotations for electives\n"
-            "- Learn about the latest student reviews\n"
-            "- Leave your review about a course\n"
-            "- Choose the language of communication (Русский or English)\n\n"
-            "To get started, use the /electives command to view available courses or /language to change the language of communication. "
-            "Click on the menu in the lower left corner to see possible commands."
-        )
+@bot.callback_query_handler(func=lambda call: call.data.startswith('set_language_'))
+def set_language(call):
+    chat_id = call.message.chat.id
+    language = call.data.split('_')[-1]
+    user_languages[chat_id] = language
+    confirmation = "Language changed to English." if language == 'en' else "Язык изменен на русский."
+    bot.send_message(chat_id, confirmation)
 
-# Получение всех элективов по типу
-def get_all_electives(elective_type: str, db: Session):
-    stmt = select(Elective).where(Elective.elective_type == elective_type)
-    electives = db.execute(stmt).scalars().all()
-    return electives
 
-# Получение информации об элективе
-def get_elective_info(elective_id: str, db: Session):
-    stmt = select(Elective).options(joinedload(Elective.reviews)).where(Elective.unique_id == elective_id)
-    elective = db.execute(stmt).scalars().first()
-    if elective:
-        return {
-            'name': elective.name,
-            'description': elective.description,
-            'reviews': [review.text for review in elective.reviews]
-        }
-    return None
+@bot.message_handler(commands=['language'])
+def choose_language(message):
+    chat_id = message.chat.id
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Русский", callback_data='set_language_ru'))
+    markup.add(types.InlineKeyboardButton("English", callback_data='set_language_en'))
+    bot.send_message(chat_id, "Выберите язык:", reply_markup=markup)
 
-# Обработчик команды /electives для выбора типа элективов
+
 @bot.message_handler(commands=['electives'])
 def choose_elective_type(message):
     chat_id = message.chat.id
+    language = get_language(chat_id)
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton("📚 PEP", callback_data='PEP'))
     markup.add(types.InlineKeyboardButton("📖 RIM", callback_data='RIM'))
-    bot.send_message(chat_id, "Выберите тип элективов:", reply_markup=markup)
+    elective_choice_message = "Выберите тип элективов:" if language == 'ru' else "Choose the type of electives:"
+    bot.send_message(chat_id, elective_choice_message, reply_markup=markup)
 
-# Обработчик выбора типа элективов
+
 @bot.callback_query_handler(func=lambda call: call.data in ['PEP', 'RIM'])
 def handle_elective_type_selection(call):
     chat_id = call.message.chat.id
     user_elective_selections[chat_id] = call.data
+    language = get_language(chat_id)
 
-    session = Session()
-    electives = session.query(Elective).filter(Elective.elective_type == call.data).all()
-    session.close()
+    with Session() as session:
+        electives = session.query(Elective).filter(Elective.elective_type == call.data).all()
 
-    response = show_electives(electives)
-    response += "\n\nВведите уникальный номер электива, чтобы получить его информацию:"
+    response = show_electives(electives, language)
+    elective_prompt = "\n\nВведите уникальный номер электива, чтобы получить его информацию:"
+    response += translate_message(elective_prompt, language)
     bot.send_message(chat_id, response)
 
-# Обработчик выбора электива
+
 @bot.message_handler(func=lambda message: message.text.isdigit())
 def handle_elective_selection(message):
     chat_id = message.chat.id
     unique_id = int(message.text)
-
-    session = Session()
+    language = get_language(chat_id)
     elective_type = user_elective_selections.get(chat_id)
 
     if not elective_type:
         bot.send_message(chat_id, "Пожалуйста, выберите тип элективов перед вводом уникального номера.")
-        session.close()
         return
 
-    elective = session.query(Elective).options(joinedload(Elective.reviews)).filter(
-        Elective.unique_id == unique_id).first()
+    with Session() as session:
+        elective = session.query(Elective).options(joinedload(Elective.reviews)).filter(
+            Elective.unique_id == unique_id).first()
+
     if elective:
         response = f"**{elective.name}**\n\n{elective.description}\n\nПоследние отзывы:\n"
-
-        # Форматирование отзывов с отступами и нумерацией
-        for i, review in enumerate(elective.reviews[-5:], 1):
-            response += f"{i}) {review.text}\n"
-
+        response += "\n".join(f"{i}) {review.text}" for i, review in enumerate(elective.reviews[-5:], 1))
         response += "\nХотите оставить отзыв? Напишите ваш отзыв ниже или нажмите /electives для возврата."
-        bot.send_message(chat_id, response)
-        user_reviews[chat_id] = unique_id  # Сохраняем уникальный ID электива для отзыва
+        bot.send_message(chat_id, translate_message(response, language))
+        user_reviews[chat_id] = unique_id
     else:
         bot.send_message(chat_id, "Электив с таким уникальным ID не найден.")
 
-    session.close()
 
-# Обработчик отзывов
 @bot.message_handler(func=lambda message: message.chat.id in user_reviews)
 def leave_review(message):
     chat_id = message.chat.id
     unique_id = user_reviews[chat_id]
     review_text = message.text
     user_name = message.from_user.username or message.from_user.first_name
+    language = get_language(chat_id)
 
-    # Создаем клавиатуру с кнопками "Да" и "Нет"
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton("✅ Да", callback_data=f'confirm_review_{unique_id}_{review_text}'))
     markup.add(types.InlineKeyboardButton("❌ Нет", callback_data='cancel_review'))
-
-    # Отправляем сообщение с просьбой подтвердить отзыв
-    bot.send_message(chat_id, f"Ваш отзыв: '{review_text}'. Подтвердите отзыв:", reply_markup=markup)
-
+    confirmation_message = f"Ваш отзыв: '{review_text}'. Подтвердите отзыв:"
+    bot.send_message(chat_id, translate_message(confirmation_message, language), reply_markup=markup)
 
 
-
-# Подтверждение отзыва
 @bot.callback_query_handler(func=lambda call: call.data.startswith('confirm_review_'))
 def confirm_review(call):
     chat_id = call.message.chat.id
     data = call.data.split('_')
 
-    # Убедитесь, что у вас есть уникальный ID электива и текст отзыва
-    if len(data) < 3:
-        bot.send_message(chat_id, "Произошла ошибка. Попробуйте оставить отзыв снова.")
-        return
-
-    unique_id = data[2]  # Предполагаем, что третий элемент - это текст отзыва, который содержит пробелы
-    review_text = '_'.join(data[3:])  # Объединяем оставшиеся части как текст отзыва
-
+    unique_id, review_text = data[2], '_'.join(data[3:])
     user_name = call.from_user.username or call.from_user.first_name
 
-    session = Session()
-    elective = session.query(Elective).filter(Elective.unique_id == int(unique_id)).first()
+    with Session() as session:
+        elective = session.query(Elective).filter(Elective.unique_id == int(unique_id)).first()
 
-    if elective:
-        new_review = Review(
-            elective_id=elective.id,
-            user_name=user_name,
-            text=review_text,
-            date_created=str(datetime.now())
-        )
-        session.add(new_review)
-        session.commit()
-        session.close()
-        bot.send_message(chat_id, "Ваш отзыв успешно оставлен!")
-    else:
-        bot.send_message(chat_id, "Ошибка при оставлении отзыва. Электив не найден.")
+        if elective:
+            new_review = Review(
+                elective_id=elective.id,
+                user_name=user_name,
+                text=review_text,
+                date_created=str(datetime.now())
+            )
+            session.add(new_review)
+            session.commit()
 
-    # Удаляем запись о выбранном элективе
-    del user_reviews[chat_id]
+    language = get_language(chat_id)
+    bot.send_message(chat_id, translate_message("Ваш отзыв успешно оставлен!", language))
+    user_reviews.pop(chat_id, None)
 
 
-
-
-# Обработчик отмены отзыва
 @bot.callback_query_handler(func=lambda call: call.data == 'cancel_review')
 def cancel_review(call):
     chat_id = call.message.chat.id
-    bot.send_message(chat_id, "Отзыв отменен. Вы можете продолжить выбирать элективы или оставить другой отзыв или нажмите /electives для возврата.")
-    del user_reviews[chat_id]
+    bot.send_message(chat_id, translate_message("Отзыв отменен.", get_language(chat_id)))
+    user_reviews.pop(chat_id, None)
 
 
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    chat_id = message.chat.id
+    help_text = (
+        "Я могу помочь вам с выбором элективов. Вот основные команды:\n"
+        "/electives - Посмотреть доступные элективы\n"
+        "/language - Изменить язык общения\n"
+        "/help - Получить помощь по использованию бота\n"
+    )
+    bot.send_message(chat_id, translate_message(help_text, get_language(chat_id)))
 
-def show_electives(electives):
-    message = "Доступные элективы:\n"
-    for elective in electives:
-        message += f"{elective.unique_id}: {elective.name}\n"  # Добавляем уникальный ID перед названием
-    return message
 
-
-bot.polling()
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
