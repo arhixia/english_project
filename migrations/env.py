88622b288b52a18dc6a886379b99b0d587cca33d
vladimir <128848265+arhixia@users.noreply.github.com>
2024-11-21 16:23:25 +0500

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,8 +1,12 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+from alembic.config import Config
 from alembic import context
 
 from src.config import DB_HOST, DB_USER,DB_NAME,DB_PORT,DB_PASS
@@ -10,7 +14,7 @@ from src.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-config = context.config
+config = Config("C:\\Users\\User\\PycharmProjects\\English_project\\alembic.ini")
 section = config.config_ini_section
 config.set_section_option(section,"DB_HOST",DB_HOST)
 config.set_section_option(section,"DB_USER",DB_USER)
